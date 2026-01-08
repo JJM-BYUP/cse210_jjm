7a8503c89790222c5bdc9d2c99ecd3e9e1759e71
JJM-BYUP <jmerritt@byupathway.edu>
2026-01-07 17:59:32 -0700

    Core Requirement 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (gradePercentage < 90 && gradePercentage >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (gradePercentage < 80 && gradePercentage >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (gradePercentage < 70 && gradePercentage >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
