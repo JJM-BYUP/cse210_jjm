3da7ff0439d9587c98e8c3ef7a39370d74775549
JJM-BYUP <jmerritt@byupathway.edu>
2026-01-09 06:53:26 -0700

    Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,41 @@ using System;
 
 class Program
 {
+    // DisplayWelcome function
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    // PromptUserName
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    //PromptUserNumber
+    static float PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        float number = float.Parse(Console.ReadLine());
+        return number;
+    }
+
+    //SquareNumber
+    static float SquareNumber(float number)
+    {
+        float sqrNumber = number * number;
+        return sqrNumber;
+    }
+
+    // Call functions and DisplayResult (main)
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        string userName = PromptUserName();
+        float userNum = PromptUserNumber();
+        float sqrNum = SquareNumber(userNum);
+        Console.WriteLine($"Hello {userName}, the square of your number is {sqrNum}");
     }
 }
