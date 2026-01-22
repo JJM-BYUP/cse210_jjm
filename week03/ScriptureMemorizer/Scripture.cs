004c062e6e9d174f52e0849db4e87d84e6f34b0c
JJM-BYUP <jmerritt@byupathway.edu>
2026-01-21 18:02:17 -0700

    made member variables private

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 
 class Scripture
 {
-    Reference _reference = new Reference("John", 3, 16);
-    List<Word> _words = new List<Word>();
+    private Reference _reference = new Reference("John", 3, 16);
+    private List<Word> _words = new List<Word>();
 
     public Scripture(Reference Reference, string text)
     {
