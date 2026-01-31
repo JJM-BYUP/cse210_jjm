317c8736efafc37c3ca4e9f33845d9854205a5e9
JJM-BYUP <jmerritt@byupathway.edu>
2026-01-31 11:43:15 -0700

    Runs with expected output - has Comment List - missing Video List

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -9,53 +9,48 @@ class Program
         /*  Create at least 3-4 video instances with comments
                 Sets appropriate values
                 For each video: adds 3-4 comments
-                Comments need commenter name and comment text
+                    Comments need commenter name and comment text
+                Put each video instance in a List
         */
+
         // Put each video in a list
         List<string> videos = new List<string>();
 
+        //VIDEO 1:
         Comment comment1a = new Comment("@deedoyle4069", "I'm 85..... THIS woman is fantastic! She says it the way things ARE. What a gal! I hope to be as delightfully humorous!");
-        
         Comment comment1b = new Comment("@sallyharrington4569", "She is hysterical!! What an amazing woman.!!!");
         Comment comment1c = new Comment("@user-rd8ui9fn1x", "Still love Sis Hinckley, she was a class act!");
         Video vid1 = new Video("Sister Marjorie Hinckley’s Humor", "Declare My Word", 259, comment1a, comment1b, comment1c);
-        
-        
+
+        vid1.DisplayVidInfo();
+
+
+        //VIDEO 2:
         Comment comment2a = new Comment("@angelamuircroft5181", "Jeanne may be gone, but her ability to make us laugh will always be her legacy.");
         Comment comment2b = new Comment("@janicew6222", "I never tire of hearing this funny lady, she is missed.");
         Comment comment2c = new Comment("@lawana960", "Jeanne is one of the few people who can make me laugh till I cry. I'm so thankful her family is still putting up these videos");
         Video vid2 = new Video("Left Brain vs. Right Brain", "Jeanne Robertson", 277, comment2a, comment2b, comment2c);
 
+        vid2.DisplayVidInfo();
 
-        // Comment comment7 = new Comment("@topazblahblah", "This is a great message. Be successful and honorable no matter what you do.");
-        // Comment comment8 = new Comment("@KwadwoConda", "I loved this as well! Thanks for your wise words, Elder D. Todd Christofferson. I agree 100%.");
-        // Comment comment9 = new Comment("@glo_moments", "In exercising faith, we don't have to know it all. We just have to trust in Him.");
-        // Comment comment10 = new Comment("@best1213best", "Things I wish i get learned when I was in high school or college at least.");
-        // Video vid3 = new Video("God guides us one step at a time", "@churchofjesuschrist", 120, comment7);
 
-        vid1.AddVidCom();
+        //VIDEO 3:
+        Comment comment3a = new Comment("@topazblahblah", "This is a great message. Be successful and honorable no matter what you do.");
+        Comment comment3b = new Comment("@KwadwoConda", "I loved this as well! Thanks for your wise words, Elder D. Todd Christofferson. I agree 100%.");
+        Comment comment3c = new Comment("@glo_moments", "In exercising faith, we don't have to know it all. We just have to trust in Him.");
+        Video vid3 = new Video("God guides us one step at a time", "@churchofjesuschrist", 120, comment3a, comment3b, comment3c);
 
-        string video1 = Convert.ToString(vid1);
-        string video2 = Convert.ToString(vid2);
+        vid3.DisplayVidInfo();
+
+
+        //string video1 = Convert.ToString(vid1);
+        //string video2 = Convert.ToString(vid2);
         //string video3 = Convert.ToString(vid3._comments);
-        videos.Add(video1);
-        videos.Add(video2);
+
+        //videos.Add(video1);
+        //videos.Add(video2);
         //videos.Add(video3);
 
-        
-
-        // DISPLAY:
-        /* Iterate through the list of videos
-            For each one display:
-                title, author, length, number of comments (from method), list
-                all comments for that video - USE NumOfComments(); for iteration
-        */
-        foreach (string vid in videos)
-        {
-            Console.WriteLine(vid);
-        }
-
-        Console.WriteLine();
     }
 }
 
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -15,7 +15,7 @@ public class Video
     private Comment _vidComment1c;
 
     // Constructor
-    public Video(string _title, string _author, int _length, Comment vidComment1a, Comment vidComment1b, Comment vidComment1c)  //
+    public Video(string _title, string _author, int _length, Comment vidComment1a, Comment vidComment1b, Comment vidComment1c)  
     {
         Title = _title;
         Author = _author;
@@ -53,48 +53,65 @@ public class Video
         }
     }
 
-    // public Comment VidComment1b
-    // {
-        // get { return _vidComment1b; }
-        // set
-        // {
-            // _vidComment1b = value;
-        // }
-    // }
+    public Comment VidComment1b
+    {
+        get { return _vidComment1b; }
+        set
+        {
+            _vidComment1b = value;
+        }
+    }
 
-    // public Comment VidComment1c
-    // {
-        // get { return _vidComment1c; }
-        // set
-        // {
-            // _vidComment1c = value;
-        // }
-    // }
+    public Comment VidComment1c
+    {
+        get { return _vidComment1c; }
+        set
+        {
+            _vidComment1c = value;
+        }
+    }
 
     // Store a List of comments
     public List<string> _comments = new List<string>();
     public void AddVidCom()
     {
+        //Adds a video's comments to a list
         string vidCom = Convert.ToString(_vidComment1a.Comments());
+        string vidCom2 = Convert.ToString(_vidComment1b.Comments());
+        string vidCom3 = Convert.ToString(_vidComment1c.Comments());
         _comments.Add(vidCom);
-        foreach (string vc in _comments)
+        _comments.Add(vidCom2);
+        _comments.Add(vidCom3);
+
+        Console.WriteLine("VIDEO COMMENTS:");
+        foreach (string vCom in _comments)
         {
-            Console.WriteLine(vc);
+            Console.WriteLine(vCom);
+            Console.WriteLine();
         }
+        
     }
 
     public int NumOfComments()
     {
-        // This method is to return the number of comments
+        // Returns the number of comments for a video
         int comLength = _comments.Count();
         return comLength;
     }
 
-    public string Display()
+    public void DisplayVidInfo()
     {
-        int commentLen = NumOfComments();
-        string vidInfo = $"{_title}\n{_author}\n{_length}";
+        /*  For each video display:
+                title, author, length, number of comments (from method), list
+                all comments for that video - USE NumOfComments(); for iteration
+        */
+
+        Console.WriteLine("**************************************************");
+        string vidInfo = $"VIDEO: {_title}\nPOSTED BY: {_author}\nVIDEO LENGTH (in seconds): {_length}";
+        Console.WriteLine(vidInfo);
+        Console.WriteLine();
         AddVidCom();
-        return vidInfo;
+        Console.WriteLine("**************************************************");
+        Console.WriteLine();
     }
 }
