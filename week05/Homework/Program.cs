7e1b8d805440af6667349f146beaf9a3557be38c
JJM-BYUP <jmerritt@byupathway.edu>
2026-02-03 17:11:01 -0700

    Committing what I have

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,8 +5,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        //ClassAssignment student1 = new ClassAssignment();
-        
-        Console.WriteLine("Hello World!");
+        // Assignment assignment = new Assignment();
+        // assignment._studentName = "Joe Shmo";
+        // assignment._topic = "Multiplication";
+        // Console.WriteLine(assignment.GetSummary());
+
+        // MathAssignment math = new MathAssignment();
+        // math._studentName = "Peter Pumpkineater";
+        // math._topic = "Complex Equations";
+        // math._textbookSection = "Word Problems";
+        // math._problems = "1-12";
+        // Console.WriteLine(math.GetHomeworkList());
+
+    
     }
 }
