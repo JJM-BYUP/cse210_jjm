63ce31ef49c32537b8797e42b53483cafb4b226a
JJM-BYUP <jmerritt@byupathway.edu>
2026-02-10 06:26:31 -0700

    Shapes program finished and working

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Circle : Shape
+{
+    private double _raduis;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _raduis = radius;
+    }
+
+    public override double GetArea()
+    {
+        return _raduis * Math.PI;
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("yellow", 4);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("blue", 5, 8);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("green", 6);
+        shapes.Add(s3);
+    
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"This is a {s.GetColor()} shape with an area of {s.GetArea()}");
+        }
     }
 }
